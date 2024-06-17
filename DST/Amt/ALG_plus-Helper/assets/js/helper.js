888e585823133676bde1caee3e9d1263c4954734
mrmll3 <37754601+mrmll3@users.noreply.github.com>
2024-06-17 16:21:16 +0200

    Add files via upload

---
+++ DST/Amt/ALG_plus-Helper/assets/js/helper.js
@@ -0,0 +1,10 @@
+
+// sidenavigation
+
+function toggleNav() {
+    if (!(document.getElementById("mySidenav").style.width === "450px")) {
+        document.getElementById("mySidenav").style.width = "450px";
+    } else {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+}
